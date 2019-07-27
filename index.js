5de3685c5ad49bd102b75eb34c84c1e07f909a77
Azim Javed <thatazimjaved@gmail.com>
2019-07-27 15:09:46 +0530

    Torrents : Start, State

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const path = require('path')
 const PORT = process.env.PORT || 3000;
 
+const torrent = require('./util/Torrent');
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
@@ -21,6 +23,29 @@ app.engine('hbs', hbs({
         __dirname + '/pages/partials'
     ]
 }))
+
+
+
+
+
+/* --------------------------   TORRENTS  --------------------------- */
+
+app.post("/fetchTorrentState", (req,res) =>{
+    /* We'd make this request from the front end, for every say, 1 second */
+    var torrentState = torrent.torrentState(req.body.torrentId)      
+    res.json(torrentState);            
+})
+
+app.post('/addTorrent', (req, res)=> {
+    let status = torrent.addTorrent(res.body.torrentId)
+    if(status){
+        res.sendStatus(200)
+    }
+    else {
+        res.sendStatus(500)
+    }
+    /* The response should be suitable, and not only statuses */
+})
 
 app.listen(PORT, ()=>{
     console.log("Server listening on port "+ PORT);
---
+++ pages/js/torrent.js
@@ -0,0 +1,22 @@
+var torrentState;
+function onSubmit() {
+    var torrentId = document.getElementById("torrentId").value
+    localStorage.setItem("torrentId", torrentId)
+    document.getElementById("torrentForm").submit();
+}
+
+function fetchState() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+        if(this.readyState === 4  && this.status === 200){
+            torrentState = request.response;
+        }
+    }
+    request.responseType = "json"
+    request.open("POST", "/fetchTorrentState", true);
+    request.send({
+        torrentId : localStorage.getItem("torrentId")
+    });
+}
+
+setTimeout(fetchState, 1000);
